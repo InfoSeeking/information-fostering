e8013b548f003bd0b2813564831d14641ff65ede
mmitsui <mmitsui@scarletmail.rutgers.edu>
2018-12-19 23:51:29 -0500

    SSH/MySQL bug fixes

--- src/data/dbconnect.py
+++ src/data/dbconnect.py
@@ -1,8 +1,11 @@
 from __future__ import print_function
-from sshtunnel import SSHTunnelForwarder
+import paramiko
+from paramiko import SSHClient
 import mysql.connector
 import configparser
+from sshtunnel import SSHTunnelForwarder
 from sqlalchemy import create_engine
+import pymysql
 
 
 def connect_with_config(path_to_config=None):
@@ -21,6 +24,20 @@ def connect_with_config(path_to_config=None):
         return (None,create_engine('mysql+mysqlconnector://%s:%s@%s/%s'%(username_mysql,password_mysql,host,dbname)))
         #return pyodbc.connect('DRIVER={SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s'%(host,dbname,username_mysql,password_mysql))
     else:
+
+        # ssh = SSHClient()
+        # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # ssh.connect(config['MYSQL']['address'], port=22, username=config['MYSQL']['username_ssh'], password=config['MYSQL']['password_ssh'])
+
+        # import time
+        # time.sleep(1)
+
+        # conn = pymysql.connect(host='127.0.0.1',
+        #            user=username_mysql,
+        #            passwd=password_mysql,
+        #            port=3306,
+        #            db=dbname)
+
         server = SSHTunnelForwarder(
             (config['MYSQL']['address'], 22),
             ssh_username=config['MYSQL']['username_ssh'],
@@ -28,11 +45,16 @@ def connect_with_config(path_to_config=None):
             remote_bind_address=('127.0.0.1', 3306)
         )
 
+
         server.start()
+
         import time
         time.sleep(1)
+
         local_port = str(server.local_bind_port)
         engine = create_engine('mysql+mysqlconnector://%s:%s@%s:%s/%s'%(username_mysql,password_mysql,host_mysql,local_port,dbname))
+
+
 
         return (server,engine)
 
